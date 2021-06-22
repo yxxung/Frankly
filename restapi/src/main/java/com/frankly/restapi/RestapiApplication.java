8f9bace9b78fc6dc41b3c33017e07a2d7c7a68fb
hanpaa <48851465+hanpaa@users.noreply.github.com>
2021-06-22 16:35:29 +0900

    제목 : 주석 추가 (#1)
    세부사항: application 에 주석을 추가해였습니다.

--- restapi/src/main/java/com/frankly/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/frankly/restapi/RestapiApplication.java
@@ -3,6 +3,8 @@ package com.frankly.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//주석을 추가 하였습니다.
 @SpringBootApplication
 public class RestapiApplication {
 
