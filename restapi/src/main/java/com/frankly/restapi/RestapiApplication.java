01d4632022635aa513ddd49b7ce2a120dd59ec77
brandon <qudgh4290@gmail.com>
2021-06-22 16:43:48 +0900

    충돌 테스트

--- restapi/src/main/java/com/frankly/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/frankly/restapi/RestapiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//주석을 추가 하였습니다.
+//주석을 추가 HIHI 하였습니다.
 @SpringBootApplication
 public class RestapiApplication {
 
