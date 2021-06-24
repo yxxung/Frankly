f99457bd593c351f67681b707b2a6d3f0cc4e44b
jehyeon <s85737@gmail.com>
2021-06-24 19:56:03 +0900

    DTO, Mapper 추가

---
+++ restapi/src/main/java/com/frankly/restapi/controller/MemberInfoController.java
@@ -0,0 +1,12 @@
+package com.frankly.restapi.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/infos")
+public class MemberInfoController {
+
+}
