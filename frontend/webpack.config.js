5732490ee26ccb6510dcbba13f7dea3375a56a23
ByungHo <qudgh4290@gmail.com>
2021-06-26 17:26:03 +0900

    오류생겨서 커밋전에 수정하다가 리액트 실행 안되던 문제 해결

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
           '@babel/preset-react'
         ],
         plugins: [
-          "@babel/plugin-transform-runtime", // asinc / await 문제 해결하려고 설치 (미해결)
+          // "@babel/plugin-transform-runtime", // asinc / await 문제 해결하려고 설치 (미해결)
           '@babel/plugin-proposal-class-properties',
           'react-refresh/babel', // 설치를 안해도 리로딩되지만 핫리로딩이랑 리로딩은 다르다. -> 브라우저 새로고침되면 기존 데이터가 다 날라간다.
         ],
