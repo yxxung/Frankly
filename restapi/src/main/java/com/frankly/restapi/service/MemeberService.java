01420693f33d38f62dad4fbadcac7c47e4b9c216
jehyeon <s85737@gmail.com>
2021-06-27 16:26:36 +0900

    프로토타입 service 로그 코드 삽입

--- restapi/src/main/java/com/frankly/restapi/service/MemeberService.java
+++ restapi/src/main/java/com/frankly/restapi/service/MemeberService.java
@@ -3,18 +3,21 @@ package com.frankly.restapi.service;
 import com.frankly.restapi.domain.MemberDTO;
 import com.frankly.restapi.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class MemeberService implements MemberServiceInterface {
 
     private final MemberMapper mapper;
 
     @Override
     public List<MemberDTO> memberList() throws Exception{
+        log.info("getList");
         return mapper.memberList();
     }
 
