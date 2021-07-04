6c4f80334ba2fe5a9ba10eabc00f23824af4a646
jehyeon <s85737@gmail.com>
2021-07-04 17:57:27 +0900

    authenticationManager implement 하여 custom후 로그인 인증 구현, api url 변경, url접근 인증필요 변경

--- restapi/src/main/java/com/frankly/restapi/controller/JwtAuthenticationController.java
+++ restapi/src/main/java/com/frankly/restapi/controller/JwtAuthenticationController.java
@@ -4,26 +4,26 @@ import com.frankly.restapi.config.JwtTokenUtil;
 import com.frankly.restapi.service.JwtUserDetailsService;
 import com.frankly.restapi.domain.JwtRequest;
 import com.frankly.restapi.domain.JwtResponse;
+import com.frankly.restapi.service.UserAuthProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/auth")
 public class JwtAuthenticationController {
 
+
     @Autowired
-    private AuthenticationManager authenticationManager;
+    private UserAuthProvider userAuthProvider;
 
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
@@ -31,25 +31,31 @@ public class JwtAuthenticationController {
     @Autowired
     private JwtUserDetailsService userDetailsService;
 
-    @RequestMapping(value = "/signin", method = RequestMethod.POST)
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/signin")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
 
-        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-
-        final UserDetails userDetails = userDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
-
-        final String token = jwtTokenUtil.generateToken(userDetails);
-
-        return ResponseEntity.ok(new JwtResponse(token, null, null));
-    }
-    private void authenticate(String username, String password) throws Exception {
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            userAuthProvider.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+
+            UserDetails userDetails = userDetailsService
+                    .loadUserByUsername(authenticationRequest.getUsername());
+            String token = jwtTokenUtil.generateToken(userDetails);
+
+
+            return ResponseEntity.ok(new JwtResponse(token, null, null));
+
         } catch (DisabledException e) {
             throw new Exception("USER_DISABLED", e);
         } catch (BadCredentialsException e) {
             throw new Exception("INVALID_CREDENTIALS", e);
         }
+
+
     }
+
+
 }
