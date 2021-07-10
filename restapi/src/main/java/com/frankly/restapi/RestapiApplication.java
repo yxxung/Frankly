12ac7ee5ec08f3c98db561db3b804c8f0596b09c
jehyeon <s85737@gmail.com>
2021-07-10 19:33:38 +0900

    다시 jar배포로 되돌리기 개발용 서버 메모리 부족으로 jenkins 해지

--- restapi/src/main/java/com/frankly/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/frankly/restapi/RestapiApplication.java
@@ -3,17 +3,9 @@ package com.frankly.restapi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestapiApplication extends SpringBootServletInitializer {
-
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-        return builder.sources(RestapiApplication.class);
-    }
+public class RestapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestapiApplication.class, args);
