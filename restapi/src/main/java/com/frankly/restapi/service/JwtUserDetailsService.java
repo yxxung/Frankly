88940a3d84e6fe3c7caa9eda2120889e9305cebb
jehyeon <s85737@gmail.com>
2021-07-10 15:33:26 +0900

    주석수정

--- restapi/src/main/java/com/frankly/restapi/service/JwtUserDetailsService.java
+++ restapi/src/main/java/com/frankly/restapi/service/JwtUserDetailsService.java
@@ -10,10 +10,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 
 //DB에서 UserDetail 받아오는 Service
 //authenticationManager에서 사용.
