cc3197fdd84aae12727956d4b8636ae2d7533b5b
jehyeon <s85737@gmail.com>
2021-07-10 19:19:52 +0900

    war 배포 변경

--- restapi/src/main/java/com/frankly/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/frankly/restapi/RestapiApplication.java
@@ -3,9 +3,17 @@ package com.frankly.restapi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestapiApplication {
+public class RestapiApplication extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(RestapiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestapiApplication.class, args);
