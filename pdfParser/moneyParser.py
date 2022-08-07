96af32e1aea50fc3a1582a0a33a1230604e2ae4e
DESKTOP-8MU859D\01064223476 <codehanpa@gmail.com>
2022-08-07 12:00:43 +0900

    테이블 인식

--- pdfParser/moneyParser.py
+++ pdfParser/moneyParser.py
@@ -1,11 +1,24 @@
-# import pdfplumber
-# f = open("parsed.txt",'w')
-#
-# with pdfplumber.open(r'money.pdf') as pdf:
-#     for pdf_page in pdf.pages:
-#         try:
-#             f.write(pdf_page.extract_text())
-#         except Exception as e:
-#             print(e)
-#
-# f.close
+import pdfplumber
+f = open("parsed.txt",'w')
+
+
+table_settings = {
+    "vertical_strategy" : "lines_strict",
+    "horizontal_strategy" : "lines_strict"
+}
+
+
+with pdfplumber.open(r'./money2.pdf') as pdf:
+    # page = pdf.pages[1]
+    # im = page.to_image(resolution=150)
+    # im.reset().debug_tablefinder(table_settings).save("./debug.PNG", format="PNG")
+    for pdf_page in pdf.pages:
+        try:
+            table = pdf_page.extract_table(table_settings)
+            # str = ','.join(table)
+            print(str)
+
+        except Exception as e:
+            print(e)
+
+f.close
