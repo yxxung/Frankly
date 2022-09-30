033904b563ee5e1ed8b440a42af3ff1fd68399fb
ejooss@chungbuk.ac.kr <ejooss@chungbuk.ac.kr>
2022-09-30 18:10:39 +0900

    feature - reply property

---
+++ restapi/src/main/java/com/frankly/restapi/domain/ReplyDTO.java
@@ -0,0 +1,23 @@
+package com.frankly.restapi.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class ReplyDTO {
+
+    private int replyID;
+
+    private String reply;
+
+    private int userID;
+
+    private LocalDateTime regDate;
+
+    private int boardID;
+
+
+}
