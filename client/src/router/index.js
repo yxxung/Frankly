c789c060f5819ebc3c8281266967539423627f78
yxxung <loveus011@naver.com>
2022-10-05 04:46:23 +0900

    board들 link연결

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -16,6 +16,22 @@ import EditUser from '../views/Admin/EditUser.vue'
 import PoliticianDetail from '../views/Politician/PoliticianDetail.vue'
 import WriteBoard from '../views/Board/WriteBoard.vue'
 import SeoulBoard from '../views/Board/region/SeoulBoard.vue'
+import BusanBoard from '../views/Board/region/BusanBoard.vue'
+import ChungbukBoard from '../views/Board/region/ChungbukBoard.vue'
+import ChungnamBoard from '../views/Board/region/ChungnamBoard.vue'
+import DaeguBoard from '../views/Board/region/DaeguBoard.vue'
+import DaejeonBoard from '../views/Board/region/DaejeonBoard.vue'
+import GwangjuBoard from '../views/Board/region/GwangjuBoard.vue'
+import GangwonBoard from '../views/Board/region/GangwonBoard.vue'
+import GyeongbukBoard from '../views/Board/region/GyeongbukBoard.vue'
+import GyeongnamBoard from '../views/Board/region/GyeongnamBoard.vue'
+import GyeonggiBoard from '../views/Board/region/GyeonggiBoard.vue'
+import IncheonBoard from '../views/Board/region/IncheonBoard.vue'
+import JejuBoard from '../views/Board/region/JejuBoard.vue'
+import JeonbukBoard from '../views/Board/region/JeonbukBoard.vue'
+import JeonnamBoard from '../views/Board/region/JeonnamBoard.vue'
+import UlsanBoard from '../views/Board/region/UlsanBoard.vue'
+import SejongBoard from '../views/Board/region/SejongBoard.vue'
 //import EditData from "@/views/Admin/EditData"
 
 const routes = [
@@ -69,6 +85,86 @@ const routes = [
     path: '/SeoulBoard',
     name: 'SeoulBoard',
     component: SeoulBoard
+  },
+  {
+    path: '/BusanBoard',
+    name: 'BusanBoard',
+    component: BusanBoard
+  },
+  {
+    path: '/ChungbukBoard',
+    name: 'ChungbukBoard',
+    component: ChungbukBoard
+  },
+  {
+    path: '/ChungnamBoard',
+    name: 'ChungnamBoard',
+    component: ChungnamBoard
+  },
+  {
+    path: '/DaeguBoard',
+    name: 'DaeguBoard',
+    component: DaeguBoard
+  },
+  {
+    path: '/DaejeonBoard',
+    name: 'DaejeonBoard',
+    component: DaejeonBoard
+  },
+  {
+    path: '/GangwonBoard',
+    name: 'GangwonBoard',
+    component: GangwonBoard
+  },
+  {
+    path: '/GwangjuBoard',
+    name: 'GwangjuBoard',
+    component: GwangjuBoard
+  },
+  {
+    path: '/GyeongbukBoard',
+    name: 'GyeongbukBoard',
+    component: GyeongbukBoard
+  },
+  {
+    path: '/GyeonggiBoard',
+    name: 'GyeonggiBoard',
+    component: GyeonggiBoard
+  },
+  {
+    path: '/GyeongnamBoard',
+    name: 'GyeongnamBoard',
+    component: GyeongnamBoard
+  },
+  {
+    path: '/IncheonBoard',
+    name: 'IncheonBoard',
+    component: IncheonBoard
+  },
+  {
+    path: '/JejuBoard',
+    name: 'JejuBoard',
+    component: JejuBoard
+  },
+  {
+    path: '/JeonbukBoard',
+    name: 'JeonbukBoard',
+    component: JeonbukBoard
+  },
+  {
+    path: '/JeonnamBoard',
+    name: 'JeonnamBoard',
+    component: JeonnamBoard
+  },
+  {
+    path: '/SejongBoard',
+    name: 'SejongBoard',
+    component: SejongBoard
+  },
+  {
+    path: '/UlsanBoard',
+    name: 'UlsanBoard',
+    component: UlsanBoard
   },
   {
     path: '/BoardDetail/:boardID',
--- client/src/views/Board/MainBoard.vue
+++ client/src/views/Board/MainBoard.vue
@@ -12,31 +12,31 @@
             <div class="community-list">
                 <h4>일반 커뮤니티</h4>
                 <ul>
-                    <li><a href="/HotBoard"><img src="@/assets/icon/Fire.svg" alt="">인기게시글</a></li>
-                    <li><a href="/FreeBoard"><img src="@/assets/icon/Pin.svg" alt="">자유게시판</a></li>
+                    <li><router-link to="/HotBoard"><img src="@/assets/icon/Fire.svg" alt="">인기게시글</router-link></li>
+                    <li><router-link to="/FreeBoard"><img src="@/assets/icon/Pin.svg" alt="">자유게시판</router-link></li>
                 </ul>
             </div>
 
             <div class="community-list">
                 <h4>지역 커뮤니티</h4>
                 <ul>
-                    <li><a href="/board/${board.region}"><img src="@/assets/icon/Pin.svg" alt="">서울특별시</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">부산광역시</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">대구광역시</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">인천광역시</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">광주광역시</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">대전광역시</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">울산광역시</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">세종특별자치시</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">경기도</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">강원도</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">충청북도</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">충청남도</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">전라북도</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">전라남도</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">경상북도</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">경상남도</a></li>
-                    <li><a href="#"><img src="@/assets/icon/Pin.svg" alt="">제주특별자치도</a></li>
+                    <li><router-link to="/SeoulBoard"><img src="@/assets/icon/Pin.svg" alt="">서울특별시</router-link></li>
+                    <li><router-link to="/BusanBoard"><img src="@/assets/icon/Pin.svg" alt="">부산광역시</router-link></li>
+                    <li><router-link to="/DaeguBoard"><img src="@/assets/icon/Pin.svg" alt="">대구광역시</router-link></li>
+                    <li><router-link to="/IncheonBoard"><img src="@/assets/icon/Pin.svg" alt="">인천광역시</router-link></li>
+                    <li><router-link to="/GwangjuBoard"><img src="@/assets/icon/Pin.svg" alt="">광주광역시</router-link></li>
+                    <li><router-link to="/DaejeonBoard"><img src="@/assets/icon/Pin.svg" alt="">대전광역시</router-link></li>
+                    <li><router-link to="/UlsanBoard"><img src="@/assets/icon/Pin.svg" alt="">울산광역시</router-link></li>
+                    <li><router-link to="/SejongBoard"><img src="@/assets/icon/Pin.svg" alt="">세종특별자치시</router-link></li>
+                    <li><router-link to="/GyeonggiBoard"><img src="@/assets/icon/Pin.svg" alt="">경기도</router-link></li>
+                    <li><router-link to="/GangwonBoard"><img src="@/assets/icon/Pin.svg" alt="">강원도</router-link></li>
+                    <li><router-link to="/ChungbukBoard"><img src="@/assets/icon/Pin.svg" alt="">충청북도</router-link></li>
+                    <li><router-link to="/ChungnamBoard"><img src="@/assets/icon/Pin.svg" alt="">충청남도</router-link></li>
+                    <li><router-link to="/JeonbukBoard"><img src="@/assets/icon/Pin.svg" alt="">전라북도</router-link></li>
+                    <li><router-link to="/JeonnamBoard"><img src="@/assets/icon/Pin.svg" alt="">전라남도</router-link></li>
+                    <li><router-link to="/GyeongbukBoard"><img src="@/assets/icon/Pin.svg" alt="">경상북도</router-link></li>
+                    <li><router-link to="/GyeongnamBoard"><img src="@/assets/icon/Pin.svg" alt="">경상남도</router-link></li>
+                    <li><router-link to="/JejuBoard"><img src="@/assets/icon/Pin.svg" alt="">제주특별자치도</router-link></li>
                 </ul>
             </div>
 
